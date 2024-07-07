02e7a63fe1c5ece17e463dfcfac3eaff429e2ed8
DeathDirk <dirkoflife@gmail.com>
2024-07-07 15:36:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print(5)
+print(type(5))
+print(5 / 5) # // - целочисенное деление / - деление
+print(5 % 5) # % - найти остаток от деления
+print(5 ** 2) # ** - возведение в степень
+print(2.0) # *.0 - тип данных Float. Целое чисо int
+print('Hello, world') # - тип данных str = string
+print('Hello,' + ' world') # - concatenate(склеивание) - сложение строк
+print(True, False) # boolean - логический тип данных
+print(5 > 10) # false
+print(5 == 5 and 5 != 5)  # == - равны ли, != не равны ли
+print(type(bool('5'))) # - Почему?
